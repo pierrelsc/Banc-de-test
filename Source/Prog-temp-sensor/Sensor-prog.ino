1b3654ad4f6071bffba541c3cc66b7ad451326d2
pierrelsc <pierre.laissac@orange.fr>
2020-03-04 12:41:27 +0100

    updating MS5-L datasheet

--- Source/Prog-temp-sensor/Sensor-prog.ino
+++ Source/Prog-temp-sensor/Sensor-prog.ino
@@ -3,8 +3,8 @@
 #include <OneWire.h> 
 #include <DallasTemperature.h>
 /********************************************************************/
-// Data wire is plugged into pin 2 on the Arduino 
-#define ONE_WIRE_BUS 2 
+// Data wire is plugged into pin 7 on the Arduino 
+#define ONE_WIRE_BUS 7 
 /********************************************************************/
 // Setup a oneWire instance to communicate with any OneWire devices  
 // (not just Maxim/Dallas temperature ICs) 
